2013b442536f2450751eb315557cafe7783412db
Charles Gleason <cgleason@student.42.us.org>
2018-09-06 14:07:50 -0700

    Add library

---
+++ ants.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ants.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cgleason <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/15 16:04:26 by cgleason          #+#    #+#             */
+/*   Updated: 2018/08/15 17:04:51 by cgleason         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ANTS_H
+# define ANTS_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include "libft.h"
+
+typdef struct	s_node
+{
+}				t_node;
+
+#endif
